5e2975142a5737856028fbc2a87efda21d603689
eri omi <eri.omi@gmail.com>
2022-01-26 14:11:02 +0900

    Error TestController::call does not exist _ 20220126

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Test;
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function index()
+    {
+    $items = Test::all();
+    return view('index', ['items' =>$items]);
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>COACHTECH</title>
+</head>
+<body>
+  <div>
+    <h1>Todo List</h1>
+    <input name="todolist">
+    <button class="btn btn-submit"> 追加 </button>
+
+  </div>
+
+</body>
+</html>
